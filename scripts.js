0d6e10e69142a89e88e37f837a13585ce2c1725f
Isaac Ramos <is4c3.rop@gmail.com>
2025-11-02 10:20:45 -0300

    feat:upgrade-click

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
 let points = document.querySelector('#points')
 const button  = document.querySelector('#main-button')
 
+const item1 = document.querySelector('#item1')
+
 let score = 0
+let click_power = 0
+
+item1.disabled = true
 
 button.addEventListener('click', function(){
 
-    score++
+    score = (score + click_power + 1)
 
     points.innerHTML = score
 
-    
+
+
+    if(score >= 100){
+
+        item1.disabled = false
+    }
+})
+
+
+item1.addEventListener('click', function(){
+
+    click_power++
+
+    score = score - 100
+    points.innerHTML = score
+
+
+    if(score < 100){
+
+        item1.disabled = true
+    }
 })
